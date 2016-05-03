54d20f3ec5f0c0a6c14511ae2688548bc60a9f8b
origamisama <origamisama10@gmail.com>
2016-05-03 17:04:28 +0900

    Using request context, indicate user agent.

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>こんにちは<h1>'
+    user_agent = request.headers.get('User-Agent')
+    return '''<h1>こんにちは<h1>
+    <h2>あなたのブラウザは %s です<h2>''' % user_agent
 
 @app.route('/user/<name>')
 def user(name):
