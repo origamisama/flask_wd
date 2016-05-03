2f0a90ea13c06c3f7cb1c8043277bb52478b4160
origamisama <origamisama10@gmail.com>
2016-05-03 16:38:03 +0900

    Reach 2b, again.

--- hello.py
+++ hello.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Konnichiha<h1>'
+    return '<h1>こんにちは<h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>こんにちは、%s<h1>' % name
 
 if __name__ == '__main__':
     app.run(debug=True)
