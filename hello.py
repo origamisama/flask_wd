3c7d4dbf17f24d18d2bf022c8c43ce663e5c3b2f
origamisama <origamisama10@gmail.com>
2016-05-05 11:25:36 +0900

    Start of chapter 2 Marge hello.py and manager.py and start using template.

--- hello.py
+++ hello.py
@@ -1,46 +1,18 @@
-from flask import Flask, request, make_response, redirect, abort
+from flask import Flask, render_template
+from flask.ext.script import Manager
+
 app = Flask(__name__)
+manager = Manager(app)
 
 # 基本
 @app.route('/')
 def index():
-    return '<h1>こんにちは</h1>'
+    return render_template('index.html')
 
 # URIから受け取り
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>こんにちは、%s</h1>' % name
-
-# UserAgent情報
-@app.route('/user-agent')
-def user_agent():
-    user_agent = request.headers.get('User-Agent')
-    return '<h1>あなたのブラウザは %s です</h1>' % user_agent
-
-# 返すコードを指定
-@app.route('/error')
-def error():
-    return '<h1>400を返します</h1>', 400
-
-# クッキー
-@app.route('/cookie')
-def cookie():
-    response = make_response('<h1>クッキーを使用します</h1>')
-    response.set_cookie('treasure', '555')
-    return response
-
-# リダイレクト
-@app.route('/redirect')
-def redirect_test():
-    return redirect('http://google.com')
-
-# 例外に404を返す（動かない）
-@app.route('/user_verify/<id>')
-def get_user(id):
-    user = load_user(id) #定義してないので動かない
-    if not user:
-        abort(404)
-    return '<h1>こんにちは、%s' % user.name
+    return render_template('user.html', name = name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
