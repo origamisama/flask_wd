af8ac0c967f8add45b4d5394d9a863db6b05fa7c
origamisama <origamisama10@gmail.com>
2016-05-05 12:50:24 +0900

    Testing variables in templates. dict, list, object.

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask.ext.script import Manager
+import random, datetime
 
 app = Flask(__name__)
 manager = Manager(app)
@@ -13,6 +14,23 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name = name)
+
+# 変数
+class ObjEx(object):    #オブジェクト呼び出しのためのテストクラス
+    def print_today(self):
+        return datetime.datetime.today()#今日の値を返す
+
+@app.route('/variables')
+def variables():
+    dict_ex = {'key':'キーさんです', 'key2':555}
+    list_ex = [34,'コイツです', 55.555]
+    rand = random.randint(0,2) #変数渡しのテスト
+    obj_ex = ObjEx()
+    return render_template('variables.html',
+                            mydict=dict_ex,
+                            mylist=list_ex,
+                            myintvar = rand,
+                            myobj=obj_ex)
 
 if __name__ == '__main__':
     manager.run()
