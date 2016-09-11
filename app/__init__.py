6f6438ef22fb7bbf5d64f3fe020914a07e6cc21f
origamisama <origamisama10@gmail.com>
2016-09-11 16:11:50 +0900

    Chapter 7 終了時まで コード追っかけ、やりかけだったCI環境は削除

---
+++ app/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+"""
+アプリケーションパッケージのコンストラクタ
+appをグローバルスコープで宣言するのではなく、関数内で作成することで、
+設定値の変更を容易にし、テストを行いやすくする
+
+Blueprintを使用することで、appをグローバルスコープで宣言した時と
+ほぼ同じようにrouteを扱えるようにする
+"""
+
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.mail import Mail
+from flask.ext.moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy
+from config import config
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
+
+def create_app(config_name):
+    """ appを生成するための関数（application factory)"""
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init__app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+
+    # routesとerror page はここに記述
+    from main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
---
+++ config.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+"""
+アプリケーションの設定を行うモジュール
+アプリケーションを生成するときに呼ばれ、各種パラメータを設定する
+開発、テスト、商用の3設定を想定
+"""
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__)) # このファイルが存在するディレクトリ
+
+class Config:
+    """全てのconfigで使用されるパラメータを保持"""
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True # Commit自動実行
+    MAIL_SUBJECT_PREFIX = '[Flask Web Development]' # 名称変更
+    MAIL_SENDER = 'Flask Web Development Admin <flask_wd@example.com>' # 名称変更
+    ADMIN = os.environ.get('FLASK_WD_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        """appを変数として扱い特定の初期設定を行える"""
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
+
